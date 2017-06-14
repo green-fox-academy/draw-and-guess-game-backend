9429137e0c1035a24afef25ac5898f4474d44dd7
Szilardoo <doczi.szilard@gmail.com>
2017-06-14 16:21:24 +0200

    feat(server):envirement variables added

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use(bodyParser.json());
 
 
 var config = {
-  user: 'postgres', //env var: PGUSER 
-  database: 'postgres', //env var: PGDATABASE 
-  password: 'root', //env var: PGPASSWORD 
-  host: 'localhost', // Server hosting the postgres database 
-  port: 5432, //env var: PGPORT 
-  max: 10, // max number of clients in the pool 
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+  user: user,
+  database: database,
+  password: password, 
+  host: host, 
+  port: port, 
+  max: 20, 
+  idleTimeoutMillis: 30000 
 };
 
 const pool = new pg.Pool(config);
